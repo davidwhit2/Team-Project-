e14674ba788f6ab629239c7b2c32719d10c5674f
davidwhit2 <david.whitaker2@student.uagc.edu>
2025-09-27 11:48:10 -0400

    Create ProtectedRoute.jsx

---
+++ ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+// src/auth/ProtectedRoute.jsx
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+
+export default function ProtectedRoute() {
+  const { isAuthed } = useAuth();
+  const location = useLocation();
+  return isAuthed ? <Outlet /> : <Navigate to="/login" replace state={{ from: location }} />;
+}
